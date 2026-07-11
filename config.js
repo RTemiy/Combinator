d91a1a5c18dbb1f0880d57eabe017b5344665f73
RTemiy <RTemiyGo@yandex.ru>
2026-07-11 17:39:10 +0300

    updated: sounds, barricades added: new categories

--- config.js
+++ config.js
@@ -291,7 +291,7 @@ export const CONFIG = {
 
   // System
   VERSION_KEY: 'merge_game_version',
-  GAME_VERSION: '1.2.0',
+  GAME_VERSION: '1.2.1',
   SAVE_KEY: 'merge_game_save',
   LAST_LOGIN_KEY: 'last_login_time',
   ROMAN_NUMERALS: { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V' },
